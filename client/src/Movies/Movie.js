82a672fa089b3918177844186616dc105d3f9ac4
Nick Ohman <nickohman@Nicks-MacBook-Pro.local>
2020-04-20 17:42:11 -0500

    done

--- client/src/Movies/Movie.js
+++ client/src/Movies/Movie.js
@@ -36,25 +36,7 @@ const Movie = (props) => {
   
   return (
     <MovieCard movie={movie}/>
-  //   <div className="save-wrapper">
-  //     <div className="movie-card">
-  //       <h2>{title}</h2>
-  //       <div className="movie-director">
-  //         Director: <em>{director}</em>
-  //       </div>
-  //       <div className="movie-metascore">
-  //         Metascore: <strong>{metascore}</strong>
-  //       </div>
-  //       <h3>Actors</h3>
-
-  //       {stars.map(star => (
-  //         <div key={star} className="movie-star">
-  //           {star}
-  //         </div>
-  //       ))}
-  //     </div>
-  //     <div className="save-button">Save</div>
-  //   </div>
+  //   
   )
 }
 
